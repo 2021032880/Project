a54d98486ae4e2c216ffeceeaab3826418c1dd48
tangleizhang <2279434303@qq.com>
2022-11-09 16:04:20 +0800

    这是第五周的代码作业

--- js/index.js
+++ js/index.js
@@ -34,4 +34,50 @@ $(function(){
         controlsCurrentColor:"#00ff00",//当前控制按钮的颜色
     });
 
-})
+    /* 电子书 table 切换*/
+    var $lis = $('.ebooks .ebooks-nav > li');
+    $lis.mouseover(function(){
+        // 给之间添加激活类active 把兄弟的都干掉
+        $(this).addClass('active').siblings().removeClass('active');
+        // 获取index
+        var index = $(this).index();
+        // 选中内容
+        var $ebooksList = $('.ebooks-list');
+        // 显示索引等于当前index
+        $ebooksList.eq(index).siblings('.ebooks-list').hide();
+    });
+
+    /* 新书列表手风琴效果*/
+    $('.ebooks .right-box  ul > li').mouseenter(function(){
+        // 所有兄弟：隐藏详情 显示标题
+        $(this).siblings().find('.desc').hide();
+        $(this).siblings().find('.ebooks-title').show();
+
+        // 当前：隐藏标题  显示详情
+        $(this).find('.ebooks-title').hide(); //隐藏标题
+        $(this).find('.desc').show(); //显示详情
+    });
+
+    /* 服装轮播 */
+    $('.clothes-banner').tyslide({
+        boxh:336, //盒子的高度
+        w :426, //盒子宽度
+        h :336, //图片高度
+        isShow:true,//是否显示控制器
+        isShowBtn:true,//是否显示左右按钮
+        controltop:10,//控制按钮上下偏移的位置,要将按钮向下移动   首先保证boxh 高度>图片 h
+        controlsW:20,//控制按钮宽度
+        controlsH:4,//控制按钮高度
+        controlsColor:"#d7d7d7",//普通控制按钮的颜色
+        controlsCurrentColor:"#ff6600",//当前控制按钮的颜色
+    });
+
+    /* 服装 table 切换 */
+    $('.clothe .clothes-nav li').mouseenter(function(){
+        // 导航切换
+        $(this).addClass('active').siblings('li').removeClass('active')
+        // 获取索引
+        var index = $(this).index();
+        $('.clothe .clothes-content > .clothes-list').eq(index).show().siblings('.clorhes-list').hide();
+    });
+});
