13e556ea8610a9da4662c94c55f4aa3a5332f4d3
tangleizhang <2279434303@qq.com>
2022-11-15 13:39:35 +0800

    这是第六周的项目文件

--- js/index.js
+++ js/index.js
@@ -20,7 +20,7 @@ $(function(){
         isShowNum:true //是否显示数字
     });
 
-    /*图书电子书小轮播*/ 
+    /*图书电子书轮播*/ 
     $('#ebooks-banner').tyslide({
         boxh:223,//盒子的高度
         w:332,//盒子的宽度
@@ -56,23 +56,24 @@ $(function(){
         // 当前：隐藏标题  显示详情
         $(this).find('.ebooks-title').hide(); //隐藏标题
         $(this).find('.desc').show(); //显示详情
+        
     });
 
     /* 服装轮播 */
-    $('.clothes-banner').tyslide({
-        boxh:336, //盒子的高度
-        w :426, //盒子宽度
-        h :336, //图片高度
-        isShow:true,//是否显示控制器
-        isShowBtn:true,//是否显示左右按钮
-        controltop:10,//控制按钮上下偏移的位置,要将按钮向下移动   首先保证boxh 高度>图片 h
-        controlsW:20,//控制按钮宽度
-        controlsH:4,//控制按钮高度
-        controlsColor:"#d7d7d7",//普通控制按钮的颜色
-        controlsCurrentColor:"#ff6600",//当前控制按钮的颜色
+    $("#clothes-banner").tyslide({
+        boxh: 330,//盒子的高度
+        w: 430,//盒子的宽度
+        h: 334,//图片的高度
+        isShow: true,//是否显示控制器
+        isShowBtn: true,//是否显示左右按钮
+        controltop: 10,//控制按钮上下偏移的位置,要将按钮向下移动   首先保证boxh 高度>图片 h
+        controlsW: 20,//控制按钮宽度
+        controlsH: 2,//控制按钮高度
+        controlsColor: "#d7d7d7",//普通控制按钮的颜色
+        controlsCurrentColor: "#00ff00",//当前控制按钮的颜色
     });
 
-    /* 服装 table 切换 */
+    /* 服装 tab 切换 */
     $('.clothe .clothes-nav li').mouseenter(function(){
         // 导航切换
         $(this).addClass('active').siblings('li').removeClass('active')
@@ -80,4 +81,38 @@ $(function(){
         var index = $(this).index();
         $('.clothe .clothes-content > .clothes-list').eq(index).show().siblings('.clorhes-list').hide();
     });
+   /* 推广商品切换 */
+   $('.promotion .top ul li').mouseenter(function(){
+    // 导航激活类切换
+    $(this).addClass('active').siblings().removeClass('iveact')
+    // 内容切换
+    // 获取对应的索引
+    var index = $(this).index();
+    console.log(index)
+    console.log($('.promotion .recommend-content .inner-box'))
+    $('.promotion .recommend-content .inner-box').animate({
+        'left':-index * 1190
+    },300)
+   })
+
+    /* 返回顶部*/
+    // 绑定滚动事件
+    $(document).scroll(function () {
+        // 获取距离顶部的位置
+        var topDistance = $('html,body').scrollTop();
+        // 判断
+        if (topDistance > 500) {
+            $('.backToToTop').fadeIn();
+        }
+        else {
+            $('.backToToTop').fadeOut();
+        }
+    })
+    // 返回底部
+    $('.backToToTop').click(function () {
+        $('html,body').animate({
+            scrollTop: 0
+        }, 300)
+    });
+    
 });
