54d6e669585d424443821ea48ac1febfa9f52303
tangleizhang <2279434303@qq.com>
2022-11-23 20:54:46 +0800

    这是第七周的项目作业文件

--- js/index.js
+++ js/index.js
@@ -33,7 +33,6 @@ $(function(){
         controlsColor:"#d7d7d7",//普通控制按钮的颜色
         controlsCurrentColor:"#00ff00",//当前控制按钮的颜色
     });
-
     /* 电子书 table 切换*/
     var $lis = $('.ebooks .ebooks-nav > li');
     $lis.mouseover(function(){
@@ -46,6 +45,21 @@ $(function(){
         // 显示索引等于当前index
         $ebooksList.eq(index).siblings('.ebooks-list').hide();
     });
+
+    /* 电子书推广商品切换 */
+    $('.ebooks .left-box .top ul li').mouseenter(function () {
+        //导航激活类的切换
+        $(this).addClass('active').siblings().removeClass('active')
+        //内容切换
+        //获取对应索引
+        var index = $(this).index(); // 0 => top : 0 * 1170, 1 =>top :-1 * 1170 ,3 =>left ;-2 * 1170
+        console.log(index)
+        console.log($('.promotion .content .inner-box'))
+        //上下移动
+        $('.promotion .content .inner-box').animate({
+            'top': -index * 600
+        }, 300)
+    })
 
     /* 新书列表手风琴效果*/
     $('.ebooks .right-box  ul > li').mouseenter(function(){
@@ -72,7 +86,6 @@ $(function(){
         controlsColor: "#d7d7d7",//普通控制按钮的颜色
         controlsCurrentColor: "#00ff00",//当前控制按钮的颜色
     });
-
     /* 服装 tab 切换 */
     $('.clothe .clothes-nav li').mouseenter(function(){
         // 导航切换
@@ -80,6 +93,32 @@ $(function(){
         // 获取索引
         var index = $(this).index();
         $('.clothe .clothes-content > .clothes-list').eq(index).show().siblings('.clorhes-list').hide();
+    });
+    /* 户外运动轮播 */
+    $("#sport-banner").tyslide({
+        boxh: 280,//盒子的高度
+        w: 478,//盒子的宽度
+        h: 280,//图片的高度
+        isShow: true,//是否显示控制器
+        isShowBtn: true,//是否显示左右按钮
+        controltop: 10,//控制按钮上下偏移的位置,要将按钮向下移动   首先保证boxh 高度>图片 h
+        controlsW: 20,//控制按钮宽度
+        controlsH: 2,//控制按钮高度
+        controlsColor: "#d7d7d7",//普通控制按钮的颜色
+        controlsCurrentColor: "#00ff00",//当前控制按钮的颜色
+    });
+    /* 童装轮播 */
+    $("#child-banner").tyslide({
+        boxh: 280,//盒子的高度
+        w: 478,//盒子的宽度
+        h: 280,//图片的高度
+        isShow: true,//是否显示控制器
+        isShowBtn: true,//是否显示左右按钮
+        controltop: 10,//控制按钮上下偏移的位置,要将按钮向下移动   首先保证boxh 高度>图片 h
+        controlsW: 20,//控制按钮宽度
+        controlsH: 2,//控制按钮高度
+        controlsColor: "#d7d7d7",//普通控制按钮的颜色
+        controlsCurrentColor: "#00ff00",//当前控制按钮的颜色
     });
    /* 推广商品切换 */
    $('.promotion .top ul li').mouseenter(function(){
@@ -93,7 +132,45 @@ $(function(){
     $('.promotion .recommend-content .inner-box').animate({
         'left':-index * 1190
     },300)
-   })
+   });
+
+   /* 二维码滑出效果 */
+   $('.qr-code .ticket').hover(function(){
+    // 让二维码出来
+    $('.qr-code div').stop(true).animate({
+        left:'-100px'
+    })
+   },function(){
+     // 让二维码隐藏
+     $('.qr-code div').stop(true).animate({
+        left:0
+    });
+
+   });
+   /*顶部搜索框互交*/
+   $(document).scroll(function(){
+    // 获取到顶部得距离
+    var topDistance = $('html, body').scrollTop();
+    // 判断
+    if (topDistance > 500){
+        // 如果滚动距离大于500 滑出来
+        $('.top-search-box').slideDown(300)
+    }else
+    // 否则收回去
+    $('.top-search-box').slideUp(300)
+   });
+   /* 楼梯跳转 */
+   $('.floor li').click(function(){
+    // 获取索引
+    var index = $(this).index();
+    // 选中每一个板块到顶部的偏移
+    var topOffset = $('.floorBox').eq(index).offset().top;
+    // 让滚动条滚到这个位置
+    $('html,body').animate({
+        scrollTop:topOffset - 50
+    })
+   });
+
 
     /* 返回顶部*/
     // 绑定滚动事件
@@ -102,14 +179,14 @@ $(function(){
         var topDistance = $('html,body').scrollTop();
         // 判断
         if (topDistance > 500) {
-            $('.backToToTop').fadeIn();
+            $('.backToTop').fadeIn();
         }
         else {
-            $('.backToToTop').fadeOut();
+            $('.backToTop').fadeOut();
         }
     })
-    // 返回底部
-    $('.backToToTop').click(function () {
+    // 返回顶部
+    $('.backToTop').click(function () {
         $('html,body').animate({
             scrollTop: 0
         }, 300)
